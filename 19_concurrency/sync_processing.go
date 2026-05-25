2fc12686779578d4dfbc9d350a007562b173c940
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-25 20:56:12 +0530

    feat: Explore sync processing

---
+++ 19_concurrency/sync_processing.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Food struct {
+	name     string
+	prepTime time.Duration
+}
+
+func prepOrder(food Food) {
+	fmt.Printf("Started preparing: %s...\n", food.name)
+	time.Sleep(food.prepTime * time.Second)
+	fmt.Printf("%s has been prepared\n\n", food.name)
+}
+
+func main() {
+	foods := []Food{
+		{
+			name:     "Roll",
+			prepTime: 5,
+		},
+		{
+			name:     "Pasta",
+			prepTime: 7,
+		},
+		{
+			name:     "Momo",
+			prepTime: 3,
+		},
+	}
+
+	for _, itm := range foods {
+		prepOrder(itm)
+	}
+}
