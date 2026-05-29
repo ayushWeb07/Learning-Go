fcea4954755528625549bf0cbc7abc062d83eab3
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-29 10:08:14 +0530

    feat: Use pointers for insertion sort

--- 23_misc/insertion_sort.go
+++ 23_misc/insertion_sort.go
@@ -2,19 +2,17 @@ package main
 
 import "fmt"
 
-func InsertionSort(arr []int) []int {
-	n := len(arr)
+func InsertionSort(arr *[]int) {
+	n := len(*arr)
 
 	for i := 1; i < n; i++ {
 		j := i
 
-		for j > 0 && arr[j-1] > arr[j] {
-			arr[j], arr[j-1] = arr[j-1], arr[j]
+		for j > 0 && (*arr)[j-1] > (*arr)[j] {
+			(*arr)[j], (*arr)[j-1] = (*arr)[j-1], (*arr)[j]
 			j--
 		}
 	}
-
-	return arr
 }
 
 func main() {
@@ -22,6 +20,6 @@ func main() {
 	nums := []int{5, 1, 3, 2, 4}
 	fmt.Println("Before:", nums)
 
-	sortedNums := InsertionSort(nums)
-	fmt.Println("After:", sortedNums)
+	InsertionSort(&nums)
+	fmt.Println("After:", nums)
 }
